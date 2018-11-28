bb45ef5052b2a06e126382f1bc426876939e13e3
Bart Schaap <b.c.schaap@gmail.com>
2018-11-28 22:03:52 +0100

    Initial commit

---
+++ Assets/scripts/PlayerMobility.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMobility : MonoBehaviour {
+
+    public float speed;
+
+    private void FixedUpdate()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Quaternion rotation = Quaternion.LookRotation(transform.position - mousePosition, Vector3.forward);
+        transform.rotation = rotation;
+        transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
+
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(Vector3.up * speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(Vector3.down * speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(Vector3.left * speed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(Vector3.right * speed);
+        }
+    }
+    
+}
